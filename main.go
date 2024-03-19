1920b80e59ee939989e2b6d73ebb5f263b157202
bdbrink <brendan.brink@outlook.com>
2024-03-19 16:35:59 -0500

    forcing import of clientset

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 
+	_ "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
